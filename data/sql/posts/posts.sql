8a42df92ea92bc062f65fab713f3e92f1e707335
Михаил Кварцевский <mishaquartzevsky@gmail.com>
2023-06-06 15:18:38 +0300

    added database

---
+++ data/sql/posts/posts.sql
@@ -0,0 +1,2 @@
+INSERT INTO post (title, subtitle, publish_date, featured) VALUES ('Post about traveling', 'My best post', '9/25/2015', 1);
+INSERT INTO post (title, subtitle, publish_date, featured) VALUES ('Post number 2 about traveling', 'My best number 2 post', '9/25/2015', 0);
